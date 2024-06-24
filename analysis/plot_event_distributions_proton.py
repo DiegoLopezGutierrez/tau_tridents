08c0ccc0d4924a46a1a2e8519b8b5b93fa4fc38d
Diego Lopez Gutierrez <d.lopezgutierrez@ganymede.physics.wustl.edu>
2024-06-24 10:24:05 -0500

    Modify RMiss distribution to only include ptMiss and ptMuon

--- analysis/plot_event_distributions_proton.py
+++ analysis/plot_event_distributions_proton.py
@@ -22,43 +22,48 @@ ptMiss_1tau_nucleon_p_5GeV = []
 ptMuon_1tau_nucleon_p_5GeV = []
 ptTau_1tau_nucleon_p_5GeV = []
 RMiss_1tau_nucleon_p_5GeV = []
+Fermi4Q2_1tau_nucleon_p_5GeV = []
 
 ### 1tau ; proton ; 10 GeV ###
 ptMiss_1tau_nucleon_p_10GeV = []
 ptMuon_1tau_nucleon_p_10GeV = []
 ptTau_1tau_nucleon_p_10GeV = []
 RMiss_1tau_nucleon_p_10GeV = []
+Fermi4Q2_1tau_nucleon_p_10GeV = []
 
 ### 1tau ; proton ; 20 GeV ###
 ptMiss_1tau_nucleon_p_20GeV = []
 ptMuon_1tau_nucleon_p_20GeV = []
 ptTau_1tau_nucleon_p_20GeV = []
 RMiss_1tau_nucleon_p_20GeV = []
+Fermi4Q2_1tau_nucleon_p_20GeV = []
 
 ### 1tau ; proton ; 50 GeV ###
 ptMiss_1tau_nucleon_p_50GeV = []
 ptMuon_1tau_nucleon_p_50GeV = []
 ptTau_1tau_nucleon_p_50GeV = []
 RMiss_1tau_nucleon_p_50GeV = []
+Fermi4Q2_1tau_nucleon_p_50GeV = []
 
 ### 1tau ; proton ; 200 GeV ###
 ptMiss_1tau_nucleon_p_200GeV = []
 ptMuon_1tau_nucleon_p_200GeV = []
 ptTau_1tau_nucleon_p_200GeV = []
 RMiss_1tau_nucleon_p_200GeV = []
-
+Fermi4Q2_1tau_nucleon_p_200GeV = []
 
 ############################
 #### Read Distributions ####
 ############################
 
-def read_after_skip(file_path, skip_lines=37):
+def read_after_skip(file_path, E_v, skip_lines=37):
     with open(file_path, 'r') as txtfile:
         for _ in range(skip_lines):
             next(txtfile)
         pt_miss_array = []
         pt_muon_array = []
         pt_tau_array = []
+        q2_array = []
         for line in txtfile:
             if line.startswith('16'):  # Read and store vtau transverse momentum
                 data = line.split()
@@ -72,8 +77,13 @@ def read_after_skip(file_path, skip_lines=37):
                 assert int(data[0]) == 13
                 px = float(data[2])
                 py = float(data[3])
+                pz = float(data[4])
+                E  = float(data[5])
+                M  = float(data[6])
                 pt_muon = np.sqrt(px**2 + py**2)
+                q2 = M**2 - E_v*(E - pz)  # Incoming v has E_v = pz_v and px_v = py_v = 0.
                 pt_muon_array.append(pt_muon)
+                q2_array.append(-q2)
             if line.startswith('-15'):  # Read and store tau transverse momentum
                 data = line.split()
                 assert int(data[0]) == -15
@@ -81,14 +91,14 @@ def read_after_skip(file_path, skip_lines=37):
                 py = float(data[3])
                 pt_tau = np.sqrt(px**2 + py**2)
                 pt_tau_array.append(pt_tau)
-        r_miss_array = [pt_miss / (pt_miss + pt_muon + pt_tau) for pt_miss, pt_muon, pt_tau in zip(pt_miss_array, pt_muon_array, pt_tau_array)]  # Calculate missing momentum ratio
-        return pt_miss_array, pt_muon_array, pt_tau_array, r_miss_array
+        r_miss_array = [pt_miss / (pt_miss + pt_muon) for pt_miss, pt_muon in zip(pt_miss_array, pt_muon_array)]  # Calculate missing momentum ratio
+        return pt_miss_array, pt_muon_array, pt_tau_array, r_miss_array, q2_array
 
-ptMiss_1tau_nucleon_p_5GeV, ptMuon_1tau_nucleon_p_5GeV, ptTau_1tau_nucleon_p_5GeV, RMiss_1tau_nucleon_p_5GeV = read_after_skip(EVENTS_DIR_1TAU+'/1tau_1e5Events_p_5GeV.txt')
-ptMiss_1tau_nucleon_p_10GeV, ptMuon_1tau_nucleon_p_10GeV, ptTau_1tau_nucleon_p_10GeV, RMiss_1tau_nucleon_p_10GeV = read_after_skip(EVENTS_DIR_1TAU+'/1tau_1e5Events_p_10GeV.txt')
-ptMiss_1tau_nucleon_p_20GeV, ptMuon_1tau_nucleon_p_20GeV, ptTau_1tau_nucleon_p_20GeV, RMiss_1tau_nucleon_p_20GeV = read_after_skip(EVENTS_DIR_1TAU+'/1tau_1e5Events_p_20GeV.txt')
-ptMiss_1tau_nucleon_p_50GeV, ptMuon_1tau_nucleon_p_50GeV, ptTau_1tau_nucleon_p_50GeV, RMiss_1tau_nucleon_p_50GeV = read_after_skip(EVENTS_DIR_1TAU+'/1tau_1e5Events_p_50GeV.txt')
-ptMiss_1tau_nucleon_p_200GeV, ptMuon_1tau_nucleon_p_200GeV, ptTau_1tau_nucleon_p_200GeV, RMiss_1tau_nucleon_p_200GeV = read_after_skip(EVENTS_DIR_1TAU+'/1tau_1e5Events_p_200GeV.txt')
+ptMiss_1tau_nucleon_p_5GeV, ptMuon_1tau_nucleon_p_5GeV, ptTau_1tau_nucleon_p_5GeV, RMiss_1tau_nucleon_p_5GeV, Fermi4Q2_1tau_nucleon_p_5GeV = read_after_skip(EVENTS_DIR_1TAU+'/1tau_1e5Events_p_5GeV.txt', 5)
+ptMiss_1tau_nucleon_p_10GeV, ptMuon_1tau_nucleon_p_10GeV, ptTau_1tau_nucleon_p_10GeV, RMiss_1tau_nucleon_p_10GeV, Fermi4Q2_1tau_nucleon_p_10GeV = read_after_skip(EVENTS_DIR_1TAU+'/1tau_1e5Events_p_10GeV.txt', 10)
+ptMiss_1tau_nucleon_p_20GeV, ptMuon_1tau_nucleon_p_20GeV, ptTau_1tau_nucleon_p_20GeV, RMiss_1tau_nucleon_p_20GeV, Fermi4Q2_1tau_nucleon_p_20GeV = read_after_skip(EVENTS_DIR_1TAU+'/1tau_1e5Events_p_20GeV.txt', 20)
+ptMiss_1tau_nucleon_p_50GeV, ptMuon_1tau_nucleon_p_50GeV, ptTau_1tau_nucleon_p_50GeV, RMiss_1tau_nucleon_p_50GeV, Fermi4Q2_1tau_nucleon_p_50GeV = read_after_skip(EVENTS_DIR_1TAU+'/1tau_1e5Events_p_50GeV.txt', 50)
+ptMiss_1tau_nucleon_p_200GeV, ptMuon_1tau_nucleon_p_200GeV, ptTau_1tau_nucleon_p_200GeV, RMiss_1tau_nucleon_p_200GeV, Fermi4Q2_1tau_nucleon_p_200GeV = read_after_skip(EVENTS_DIR_1TAU+'/1tau_1e5Events_p_200GeV.txt', 200)
 
 if write_dist:
     with open('ptDist_1tau_nucleon_p_5GeV.txt','w',newline='') as outfile:
@@ -149,6 +159,12 @@ RMiss_1tau_nucleon_p_5GeV_bins, RMiss_1tau_nucleon_p_5GeV_step = np.linspace(RMi
 print("RMiss:")
 print("\tmin: ", RMiss_1tau_nucleon_p_5GeV_min)
 print("\tmax: ", RMiss_1tau_nucleon_p_5GeV_max)
+Fermi4Q2_1tau_nucleon_p_5GeV_max = max(Fermi4Q2_1tau_nucleon_p_5GeV)
+Fermi4Q2_1tau_nucleon_p_5GeV_min = min(Fermi4Q2_1tau_nucleon_p_5GeV)
+Fermi4Q2_1tau_nucleon_p_5GeV_bins, Fermi4Q2_1tau_nucleon_p_5GeV_step = np.linspace(Fermi4Q2_1tau_nucleon_p_5GeV_min, Fermi4Q2_1tau_nucleon_p_5GeV_max, num=NBINS, retstep=True)
+print("Fermi4Q2:")
+print("\tmin: ", Fermi4Q2_1tau_nucleon_p_5GeV_min)
+print("\tmax: ", Fermi4Q2_1tau_nucleon_p_5GeV_max)
 
 print("---------- 10 GeV ----------")
 ptMiss_1tau_nucleon_p_10GeV_max = max(ptMiss_1tau_nucleon_p_10GeV)
@@ -175,6 +191,12 @@ RMiss_1tau_nucleon_p_10GeV_bins, RMiss_1tau_nucleon_p_10GeV_step = np.linspace(R
 print("RMiss:")
 print("\tmin: ", RMiss_1tau_nucleon_p_10GeV_min)
 print("\tmax: ", RMiss_1tau_nucleon_p_10GeV_max)
+Fermi4Q2_1tau_nucleon_p_10GeV_max = max(Fermi4Q2_1tau_nucleon_p_10GeV)
+Fermi4Q2_1tau_nucleon_p_10GeV_min = min(Fermi4Q2_1tau_nucleon_p_10GeV)
+Fermi4Q2_1tau_nucleon_p_10GeV_bins, Fermi4Q2_1tau_nucleon_p_10GeV_step = np.linspace(Fermi4Q2_1tau_nucleon_p_10GeV_min, Fermi4Q2_1tau_nucleon_p_10GeV_max, num=NBINS, retstep=True)
+print("Fermi4Q2:")
+print("\tmin: ", Fermi4Q2_1tau_nucleon_p_10GeV_min)
+print("\tmax: ", Fermi4Q2_1tau_nucleon_p_10GeV_max)
 
 print("---------- 20 GeV ----------")
 ptMiss_1tau_nucleon_p_20GeV_max = max(ptMiss_1tau_nucleon_p_20GeV)
@@ -201,6 +223,12 @@ RMiss_1tau_nucleon_p_20GeV_bins, RMiss_1tau_nucleon_p_20GeV_step = np.linspace(R
 print("RMiss:")
 print("\tmin: ", RMiss_1tau_nucleon_p_20GeV_min)
 print("\tmax: ", RMiss_1tau_nucleon_p_20GeV_max)
+Fermi4Q2_1tau_nucleon_p_20GeV_max = max(Fermi4Q2_1tau_nucleon_p_20GeV)
+Fermi4Q2_1tau_nucleon_p_20GeV_min = min(Fermi4Q2_1tau_nucleon_p_20GeV)
+Fermi4Q2_1tau_nucleon_p_20GeV_bins, Fermi4Q2_1tau_nucleon_p_20GeV_step = np.linspace(Fermi4Q2_1tau_nucleon_p_20GeV_min, Fermi4Q2_1tau_nucleon_p_20GeV_max, num=NBINS, retstep=True)
+print("Fermi4Q2:")
+print("\tmin: ", Fermi4Q2_1tau_nucleon_p_20GeV_min)
+print("\tmax: ", Fermi4Q2_1tau_nucleon_p_20GeV_max)
 
 print("---------- 50 GeV ----------")
 ptMiss_1tau_nucleon_p_50GeV_max = max(ptMiss_1tau_nucleon_p_50GeV)
@@ -227,6 +255,12 @@ RMiss_1tau_nucleon_p_50GeV_bins, RMiss_1tau_nucleon_p_50GeV_step = np.linspace(R
 print("RMiss:")
 print("\tmin: ", RMiss_1tau_nucleon_p_50GeV_min)
 print("\tmax: ", RMiss_1tau_nucleon_p_50GeV_max)
+Fermi4Q2_1tau_nucleon_p_50GeV_max = max(Fermi4Q2_1tau_nucleon_p_50GeV)
+Fermi4Q2_1tau_nucleon_p_50GeV_min = min(Fermi4Q2_1tau_nucleon_p_50GeV)
+Fermi4Q2_1tau_nucleon_p_50GeV_bins, Fermi4Q2_1tau_nucleon_p_50GeV_step = np.linspace(Fermi4Q2_1tau_nucleon_p_50GeV_min, Fermi4Q2_1tau_nucleon_p_50GeV_max, num=NBINS, retstep=True)
+print("Fermi4Q2:")
+print("\tmin: ", Fermi4Q2_1tau_nucleon_p_50GeV_min)
+print("\tmax: ", Fermi4Q2_1tau_nucleon_p_50GeV_max)
 
 print("---------- 200 GeV ----------")
 ptMiss_1tau_nucleon_p_200GeV_max = max(ptMiss_1tau_nucleon_p_200GeV)
@@ -253,6 +287,12 @@ RMiss_1tau_nucleon_p_200GeV_bins, RMiss_1tau_nucleon_p_200GeV_step = np.linspace
 print("RMiss:")
 print("\tmin: ", RMiss_1tau_nucleon_p_200GeV_min)
 print("\tmax: ", RMiss_1tau_nucleon_p_200GeV_max)
+Fermi4Q2_1tau_nucleon_p_200GeV_max = max(Fermi4Q2_1tau_nucleon_p_200GeV)
+Fermi4Q2_1tau_nucleon_p_200GeV_min = min(Fermi4Q2_1tau_nucleon_p_200GeV)
+Fermi4Q2_1tau_nucleon_p_200GeV_bins, Fermi4Q2_1tau_nucleon_p_200GeV_step = np.linspace(Fermi4Q2_1tau_nucleon_p_200GeV_min, Fermi4Q2_1tau_nucleon_p_200GeV_max, num=NBINS, retstep=True)
+print("Fermi4Q2:")
+print("\tmin: ", Fermi4Q2_1tau_nucleon_p_200GeV_min)
+print("\tmax: ", Fermi4Q2_1tau_nucleon_p_200GeV_max)
 
 ### Weights ###
 NEVENTS = 1e5
@@ -262,30 +302,35 @@ wts_ptMiss_1tau_nucleon_p_5GeV = np.ones_like(ptMiss_1tau_nucleon_p_5GeV) / (NEV
 wts_ptMuon_1tau_nucleon_p_5GeV = np.ones_like(ptMuon_1tau_nucleon_p_5GeV) / (NEVENTS * ptMuon_1tau_nucleon_p_5GeV_step)
 wts_ptTau_1tau_nucleon_p_5GeV = np.ones_like(ptTau_1tau_nucleon_p_5GeV) / (NEVENTS * ptTau_1tau_nucleon_p_5GeV_step)
 wts_RMiss_1tau_nucleon_p_5GeV = np.ones_like(RMiss_1tau_nucleon_p_5GeV) / (NEVENTS * RMiss_1tau_nucleon_p_5GeV_step)
+wts_Fermi4Q2_1tau_nucleon_p_5GeV = np.ones_like(Fermi4Q2_1tau_nucleon_p_5GeV) / (NEVENTS * Fermi4Q2_1tau_nucleon_p_5GeV_step)
 
 # 10 GeV #
 wts_ptMiss_1tau_nucleon_p_10GeV = np.ones_like(ptMiss_1tau_nucleon_p_10GeV) / (NEVENTS * ptMiss_1tau_nucleon_p_10GeV_step)
 wts_ptMuon_1tau_nucleon_p_10GeV = np.ones_like(ptMuon_1tau_nucleon_p_10GeV) / (NEVENTS * ptMuon_1tau_nucleon_p_10GeV_step)
 wts_ptTau_1tau_nucleon_p_10GeV = np.ones_like(ptTau_1tau_nucleon_p_10GeV) / (NEVENTS * ptTau_1tau_nucleon_p_10GeV_step)
 wts_RMiss_1tau_nucleon_p_10GeV = np.ones_like(RMiss_1tau_nucleon_p_10GeV) / (NEVENTS * RMiss_1tau_nucleon_p_10GeV_step)
+wts_Fermi4Q2_1tau_nucleon_p_10GeV = np.ones_like(Fermi4Q2_1tau_nucleon_p_10GeV) / (NEVENTS * Fermi4Q2_1tau_nucleon_p_10GeV_step)
 
 # 20 GeV #
 wts_ptMiss_1tau_nucleon_p_20GeV = np.ones_like(ptMiss_1tau_nucleon_p_20GeV) / (NEVENTS * ptMiss_1tau_nucleon_p_20GeV_step)
 wts_ptMuon_1tau_nucleon_p_20GeV = np.ones_like(ptMuon_1tau_nucleon_p_20GeV) / (NEVENTS * ptMuon_1tau_nucleon_p_20GeV_step)
 wts_ptTau_1tau_nucleon_p_20GeV = np.ones_like(ptTau_1tau_nucleon_p_20GeV) / (NEVENTS * ptTau_1tau_nucleon_p_20GeV_step)
 wts_RMiss_1tau_nucleon_p_20GeV = np.ones_like(RMiss_1tau_nucleon_p_20GeV) / (NEVENTS * RMiss_1tau_nucleon_p_20GeV_step)
+wts_Fermi4Q2_1tau_nucleon_p_20GeV = np.ones_like(Fermi4Q2_1tau_nucleon_p_20GeV) / (NEVENTS * Fermi4Q2_1tau_nucleon_p_20GeV_step)
 
 # 50 GeV #
 wts_ptMiss_1tau_nucleon_p_50GeV = np.ones_like(ptMiss_1tau_nucleon_p_50GeV) / (NEVENTS * ptMiss_1tau_nucleon_p_50GeV_step)
 wts_ptMuon_1tau_nucleon_p_50GeV = np.ones_like(ptMuon_1tau_nucleon_p_50GeV) / (NEVENTS * ptMuon_1tau_nucleon_p_50GeV_step)
 wts_ptTau_1tau_nucleon_p_50GeV = np.ones_like(ptTau_1tau_nucleon_p_50GeV) / (NEVENTS * ptTau_1tau_nucleon_p_50GeV_step)
 wts_RMiss_1tau_nucleon_p_50GeV = np.ones_like(RMiss_1tau_nucleon_p_50GeV) / (NEVENTS * RMiss_1tau_nucleon_p_50GeV_step)
+wts_Fermi4Q2_1tau_nucleon_p_50GeV = np.ones_like(Fermi4Q2_1tau_nucleon_p_50GeV) / (NEVENTS * Fermi4Q2_1tau_nucleon_p_50GeV_step)
 
 # 200 GeV #
 wts_ptMiss_1tau_nucleon_p_200GeV = np.ones_like(ptMiss_1tau_nucleon_p_200GeV) / (NEVENTS * ptMiss_1tau_nucleon_p_200GeV_step)
 wts_ptMuon_1tau_nucleon_p_200GeV = np.ones_like(ptMuon_1tau_nucleon_p_200GeV) / (NEVENTS * ptMuon_1tau_nucleon_p_200GeV_step)
 wts_ptTau_1tau_nucleon_p_200GeV = np.ones_like(ptTau_1tau_nucleon_p_200GeV) / (NEVENTS * ptTau_1tau_nucleon_p_200GeV_step)
 wts_RMiss_1tau_nucleon_p_200GeV = np.ones_like(RMiss_1tau_nucleon_p_200GeV) / (NEVENTS * RMiss_1tau_nucleon_p_200GeV_step)
+wts_Fermi4Q2_1tau_nucleon_p_200GeV = np.ones_like(Fermi4Q2_1tau_nucleon_p_200GeV) / (NEVENTS * Fermi4Q2_1tau_nucleon_p_200GeV_step)
 
 ##################
 #### Plotting ####
@@ -298,6 +343,7 @@ if vertical_flag:
     fig2, (ax21, ax22, ax23, ax24, ax25) = plt.subplots(1, 5, sharey=True, figsize=(60,15), tight_layout=True) # ptMuon
     fig3, (ax31, ax32, ax33, ax34, ax35) = plt.subplots(1, 5, sharey=True, figsize=(60,15), tight_layout=True) # ptTau
     fig4, (ax41, ax42, ax43, ax44, ax45) = plt.subplots(1, 5, sharey=True, figsize=(60,15), tight_layout=True) # RMiss
+    fig5, (ax51, ax52, ax53, ax54, ax55) = plt.subplots(1, 5, sharey=True, figsize=(60,15), tight_layout=True) # Fermi4Q2
 
     ### ptMiss ###
     ax11.hist(ptMiss_1tau_nucleon_p_5GeV, density=False, bins=ptMiss_1tau_nucleon_p_5GeV_bins, weights=wts_ptMiss_1tau_nucleon_p_5GeV, color='royalblue', alpha=0.3, edgecolor='black', lw=0.5)
@@ -346,13 +392,27 @@ if vertical_flag:
     ax44.hist(RMiss_1tau_nucleon_p_50GeV, density=False, bins=RMiss_1tau_nucleon_p_50GeV_bins, weights=wts_RMiss_1tau_nucleon_p_50GeV, histtype='step', color='black', alpha=1, lw=2)
     ax45.hist(RMiss_1tau_nucleon_p_200GeV, density=False, bins=RMiss_1tau_nucleon_p_200GeV_bins, weights=wts_RMiss_1tau_nucleon_p_200GeV, color='royalblue', alpha=0.3, edgecolor='black', lw=0.5)
     ax45.hist(RMiss_1tau_nucleon_p_200GeV, density=False, bins=RMiss_1tau_nucleon_p_200GeV_bins, weights=wts_RMiss_1tau_nucleon_p_200GeV, histtype='step', color='black', alpha=1, lw=2)
+    
+    ### Fermi4Q2 ###
+    ax51.hist(Fermi4Q2_1tau_nucleon_p_5GeV, density=False, bins=Fermi4Q2_1tau_nucleon_p_10GeV_bins, weights=wts_Fermi4Q2_1tau_nucleon_p_10GeV, color='royalblue', alpha=0.3, edgecolor='black', lw=0.5)
+    ax51.hist(Fermi4Q2_1tau_nucleon_p_5GeV, density=False, bins=Fermi4Q2_1tau_nucleon_p_10GeV_bins, weights=wts_Fermi4Q2_1tau_nucleon_p_10GeV, histtype='step', color='black', alpha=1, lw=2)
+    ax52.hist(Fermi4Q2_1tau_nucleon_p_10GeV, density=False, bins=Fermi4Q2_1tau_nucleon_p_10GeV_bins, weights=wts_Fermi4Q2_1tau_nucleon_p_10GeV, color='royalblue', alpha=0.3, edgecolor='black', lw=0.5)
+    ax52.hist(Fermi4Q2_1tau_nucleon_p_10GeV, density=False, bins=Fermi4Q2_1tau_nucleon_p_10GeV_bins, weights=wts_Fermi4Q2_1tau_nucleon_p_10GeV, histtype='step', color='black', alpha=1, lw=2)
+    ax53.hist(Fermi4Q2_1tau_nucleon_p_20GeV, density=False, bins=Fermi4Q2_1tau_nucleon_p_20GeV_bins, weights=wts_Fermi4Q2_1tau_nucleon_p_20GeV, color='royalblue', alpha=0.3, edgecolor='black', lw=0.5)
+    ax53.hist(Fermi4Q2_1tau_nucleon_p_20GeV, density=False, bins=Fermi4Q2_1tau_nucleon_p_20GeV_bins, weights=wts_Fermi4Q2_1tau_nucleon_p_20GeV, histtype='step', color='black', alpha=1, lw=2)
+    ax54.hist(Fermi4Q2_1tau_nucleon_p_50GeV, density=False, bins=Fermi4Q2_1tau_nucleon_p_50GeV_bins, weights=wts_Fermi4Q2_1tau_nucleon_p_50GeV, color='royalblue', alpha=0.3, edgecolor='black', lw=0.5)
+    ax54.hist(Fermi4Q2_1tau_nucleon_p_50GeV, density=False, bins=Fermi4Q2_1tau_nucleon_p_50GeV_bins, weights=wts_Fermi4Q2_1tau_nucleon_p_50GeV, histtype='step', color='black', alpha=1, lw=2)
+    ax55.hist(Fermi4Q2_1tau_nucleon_p_200GeV, density=False, bins=Fermi4Q2_1tau_nucleon_p_200GeV_bins, weights=wts_Fermi4Q2_1tau_nucleon_p_200GeV, color='royalblue', alpha=0.3, edgecolor='black', lw=0.5)
+    ax55.hist(Fermi4Q2_1tau_nucleon_p_200GeV, density=False, bins=Fermi4Q2_1tau_nucleon_p_200GeV_bins, weights=wts_Fermi4Q2_1tau_nucleon_p_200GeV, histtype='step', color='black', alpha=1, lw=2)
+
 
     ### Labels ###
     ax11.set_ylabel(r'$\nu_\mu \to \nu_\tau \tau^+ \mu^-$ (dN / bin / N)', fontsize=40)
     ax21.set_ylabel(r'$\nu_\mu \to \nu_\tau \tau^+ \mu^-$ (dN / bin / N)', fontsize=40)
     ax31.set_ylabel(r'$\nu_\mu \to \nu_\tau \tau^+ \mu^-$ (dN / bin / N)', fontsize=40)
     ax41.set_ylabel(r'$\nu_\mu \to \nu_\tau \tau^+ \mu^-$ (dN / bin / N)', fontsize=40)
-    
+    ax51.set_ylabel(r'$\nu_\mu \to \nu_\tau \tau^+ \mu^-$ (dN / bin / N)', fontsize=40)
+   
     for ax in [ax11, ax12, ax13, ax14, ax15]:
         ax.set_xlabel(r'\textbf{Missing Transverse Momentum} $p^T_{\mathrm{Miss}}$ (GeV) ', fontsize=40)
 
@@ -364,21 +424,24 @@ if vertical_flag:
     
     for ax in [ax41, ax42, ax43, ax44, ax45]:
         ax.set_xlabel(r'\textbf{Missing Momentum Ratio} $R^T_{\mathrm{Miss}}$', fontsize=40)
+    
+    for ax in [ax51, ax52, ax53, ax54, ax55]:
+        ax.set_xlabel(r'\textbf{4-Fermi Momentum Transfer} $Q^2_{\mathrm{4-Fermi}}$', fontsize=40)
 
-    for ax in [ax11, ax21, ax31, ax41]:
-        ax.text(0.90,0.90,r'\textbf{5 GeV}',ha='right',transform=ax.transAxes)
+    for ax in [ax11, ax21, ax31, ax41, ax51]:
+        ax.text(0.90,0.90,r'\textbf{5 GeV}',ha='right',transform=ax.transAxes, fontsize=30)
 
-    for ax in [ax12, ax22, ax32, ax42]:
-        ax.text(0.90,0.90,r'\textbf{10 GeV}',ha='right',transform=ax.transAxes)
+    for ax in [ax12, ax22, ax32, ax42, ax52]:
+        ax.text(0.90,0.90,r'\textbf{10 GeV}',ha='right',transform=ax.transAxes, fontsize=30)
 
-    for ax in [ax13, ax23, ax33, ax43]:
-        ax.text(0.90,0.90,r'\textbf{20 GeV}',ha='right',transform=ax.transAxes)
+    for ax in [ax13, ax23, ax33, ax43, ax53]:
+        ax.text(0.90,0.90,r'\textbf{20 GeV}',ha='right',transform=ax.transAxes, fontsize=30)
 
-    for ax in [ax14, ax24, ax34, ax44]:
-        ax.text(0.90,0.90,r'\textbf{50 GeV}',ha='right',transform=ax.transAxes)
+    for ax in [ax14, ax24, ax34, ax44, ax54]:
+        ax.text(0.90,0.90,r'\textbf{50 GeV}',ha='right',transform=ax.transAxes, fontsize=30)
 
-    for ax in [ax15, ax25, ax35, ax45]:
-        ax.text(0.90,0.90,r'\textbf{200 GeV}',ha='right',transform=ax.transAxes)
+    for ax in [ax15, ax25, ax35, ax45, ax55]:
+        ax.text(0.90,0.90,r'\textbf{200 GeV}',ha='right',transform=ax.transAxes, fontsize=30)
 
     ### Ticks ###
     locmaj = matplotlib.ticker.LogLocator(base=10.0, subs=(1.0, ), numticks=1000)
@@ -389,6 +452,8 @@ if vertical_flag:
     locmin3 = matplotlib.ticker.LogLocator(base=10.0, subs=np.arange(2, 10)*.1,numticks=2000)
     locmaj4 = matplotlib.ticker.LogLocator(base=10.0, subs=(1.0, ), numticks=1000)
     locmin4 = matplotlib.ticker.LogLocator(base=10.0, subs=np.arange(2, 10)*.1,numticks=2000)
+    locmaj5 = matplotlib.ticker.LogLocator(base=10.0, subs=(1.0, ), numticks=1000)
+    locmin5 = matplotlib.ticker.LogLocator(base=10.0, subs=np.arange(2, 10)*.1,numticks=2000)
 
     ax11.yaxis.set_major_locator(locmaj)
     ax11.yaxis.set_minor_locator(locmin)
@@ -398,6 +463,8 @@ if vertical_flag:
     ax31.yaxis.set_minor_locator(locmin3)
     ax41.yaxis.set_major_locator(locmaj4)
     ax41.yaxis.set_minor_locator(locmin4)
+    ax51.yaxis.set_major_locator(locmaj5)
+    ax51.yaxis.set_minor_locator(locmin5)
 
     ### Limits ###
     # ptMiss #
@@ -428,6 +495,13 @@ if vertical_flag:
 #    ax44.set_xlim(RMiss_1tau_nucleon_p_50GeV_min, RMiss_1tau_nucleon_p_50GeV_max)
 #    ax45.set_xlim(RMiss_1tau_nucleon_p_200GeV_min, RMiss_1tau_nucleon_p_200GeV_max)
     
+    # ptTau #
+    ax51.set_xlim(Fermi4Q2_1tau_nucleon_p_5GeV_min, Fermi4Q2_1tau_nucleon_p_5GeV_max)
+    ax52.set_xlim(Fermi4Q2_1tau_nucleon_p_10GeV_min, Fermi4Q2_1tau_nucleon_p_10GeV_max)
+    ax53.set_xlim(Fermi4Q2_1tau_nucleon_p_20GeV_min, Fermi4Q2_1tau_nucleon_p_20GeV_max)
+    ax54.set_xlim(Fermi4Q2_1tau_nucleon_p_50GeV_min, Fermi4Q2_1tau_nucleon_p_50GeV_max)
+    ax55.set_xlim(Fermi4Q2_1tau_nucleon_p_200GeV_min, Fermi4Q2_1tau_nucleon_p_200GeV_max)
+
     ax41.set_xlim(0, 1)
     ax42.set_xlim(0, 1)
     ax43.set_xlim(0, 1)
@@ -438,6 +512,7 @@ if vertical_flag:
     ax21.set_yscale('log')
     ax31.set_yscale('log')
     ax41.set_yscale('log')
+    ax51.set_yscale('log')
 
 # horizontal #
 if horizontal_flag:
@@ -520,3 +595,4 @@ fig1.savefig("../plots/ptMiss_1tau_incoh_p.png", dpi=100, bbox_inches='tight')
 fig2.savefig("../plots/ptMuon_1tau_incoh_p.png", dpi=100, bbox_inches='tight')
 fig3.savefig("../plots/ptTau_1tau_incoh_p.png", dpi=100, bbox_inches='tight')
 fig4.savefig("../plots/RMiss_1tau_incoh_p.png", dpi=100, bbox_inches='tight')
+fig5.savefig("../plots/Fermi4Q2_1tau_incoh_p.png", dpi=100, bbox_inches='tight')
