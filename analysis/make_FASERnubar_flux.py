d27ee1f0aa290d512de20cdaad03cd67430dc370
Diego Lopez Gutierrez <diego_lopezg@hotmail.com>
2024-06-25 11:38:54 -0500

    Update plots and plotting scripts. Add DIS cross sections, form factors and FASER and T2K fluxes

--- analysis/make_FASERnubar_flux.py
+++ analysis/make_FASERnubar_flux.py
@@ -9,19 +9,13 @@ from scipy.integrate import simpson
 STYLE_DIR = '../plots/styles/'
 plt.style.use(STYLE_DIR+'sty.mplstyle')
 
-FLUX_DIR = '../csv/fluxes/FASERnu/vmu/'
+FLUX_DIR = '../csv/fluxes/FASERnu/vmubar/'
 
 E_pion = []
 E_kaon = []
 E_hyperon = []
 E_charm = []
 E_bottom = []
-
-E_toni = []
-bins_toni = []
-
-E_fast_vmu = []
-E_fast_vmubar = []
 
 flux_pion = []
 flux_kaon = []
@@ -30,24 +24,14 @@ flux_charm = []
 flux_bottom = []
 flux_total = []
 
-flux_toni = []
-flux_fast_vmu = []
-flux_fast_vmubar = []
-
-pion_filename = FLUX_DIR+'FASERvmu_pion.csv'
-kaon_filename = FLUX_DIR+'FASERvmu_kaon.csv'
-hyperon_filename = FLUX_DIR+'FASERvmu_hyperon.csv'
-charm_filename = FLUX_DIR+'FASERvmu_charm.csv'
-bottom_filename = FLUX_DIR+'FASERvmu_bottom.csv'
+pion_filename = FLUX_DIR+'FASERvmubar_pion.csv'
+kaon_filename = FLUX_DIR+'FASERvmubar_kaon.csv'
+hyperon_filename = FLUX_DIR+'FASERvmubar_hyperon.csv'
+charm_filename = FLUX_DIR+'FASERvmubar_charm.csv'
+bottom_filename = FLUX_DIR+'FASERvmubar_bottom.csv'
 
 normalization = 25*25*1e-4 # 25 x 25 cm^2 to m^2
 normalization *= 150 # 150 fb^-1 luminosity
-#normalization = 1
-
-toni_filename = FLUX_DIR+'../FASERvmu_Toni.csv'
-
-fast_vmu_filename = FLUX_DIR + '../FastNeutrinoFluxSimulation/FASER_14.txt'
-fast_vmubar_filename = FLUX_DIR + '../FastNeutrinoFluxSimulation/FASER_-14.txt'
 
 with open(pion_filename,'r') as csvfile:
     data = csv.reader(csvfile, delimiter = ',') 
@@ -105,39 +89,8 @@ with open('../csv/fluxes/FASERnu/FASERvmu_bins.csv','r') as csvfile:
 bins_FASERvmu.append(8150.00)
 bins_FASERvmu = np.array(bins_FASERvmu)
 
-total_entries_toni = 0
-
-with open(toni_filename,'r') as csvfile:
-    data = csv.reader(csvfile, delimiter = ',')
-    for row in data:
-        low_E = float(row[0])
-        high_E = float(row[1])
-        N = float(row[2])
-        total_entries_toni += N
-        bins_toni.append(low_E)
-        E_toni.append(low_E+(high_E-low_E)/2)
-        flux_toni.append(N)
-
-with open(fast_vmu_filename,'r') as txtfile:
-    for line in txtfile:
-        data = line.split()
-        energy = float(data[0])
-        flux = float(data[1])    # Flux is v's assuming 150 fb^-1 and 25x25 cm^2
-        E_fast_vmu.append(energy)
-        flux_fast_vmu.append(flux)
-
-with open(fast_vmubar_filename,'r') as txtfile:
-    for line in txtfile:
-        data = line.split()
-        energy = float(data[0])
-        flux = float(data[1])    # Flux is v's assuming 150 fb^-1 and 25x25 cm^2
-        E_fast_vmubar.append(energy)
-        flux_fast_vmubar.append(flux)
-
-bins_toni.append(10000.0)
-
+######## Plotting ########  
 fig1, ax1 = plt.subplots(1, 1, figsize=(17,13), tight_layout=True)
-fig2, ax2 = plt.subplots(1, 1, figsize=(15,13), tight_layout=True)
 
 col_map = mpl.colormaps['Dark2'].resampled(6)
 rgb_col = np.linspace(0,1,6)
@@ -166,34 +119,16 @@ for e_bin, N_pion, N_kaon, N_hyperon, N_charm, N_bottom in zip(bins_pion, hist_p
 
 ax1.hist(bins_pion[:-1], bins=bins_pion, weights=hist_total, histtype='step', color=c_total,lw=5, alpha=1, label='total')
 
-# Comparison with other FASER flux
-hist_toni, bins_toni, patches_toni = ax2.hist(E_toni, bins=bins_toni, weights=flux_toni, histtype='step', color='red',lw=4, alpha=1, label='Toni')
-ax2.plot(E_fast_vmu, flux_fast_vmu, '-', label=r'$\nu_\mu$', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
-ax2.plot(E_fast_vmubar, flux_fast_vmubar, '-', label=r'$\bar{\nu}_\mu$', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
-ax2.hist(bins_pion[:-1], bins=bins_pion, weights=[total*normalization for total in hist_total], histtype='step', color=c_total, lw=5, alpha=1, label='total')
-
 ax1.set_xlabel(r'\textbf{Neutrino Energy} (GeV)')
-#ax1.set_ylabel(r'$\frac{\mathrm{d}\Phi}{\mathrm{d}E}$ ($N_\nu$ / m$^{2}$ GeV fb$^{-1}$)')
 ax1.set_ylabel(r'$\mathrm{d}\Phi$ ($N_\nu$ / m$^2$ fb$^{-1}$)')
 ax1.set_xscale('log')
 ax1.set_yscale('log')
 ax1.set_ylim(1e6,1e12)
 ax1.set_xlim(1e1,5e4)
 ax1.legend(loc='upper right')
-ax1.set_title(r"\textbf{FASER}$\nu$ $\nu_\mu$ \textbf{flux}", fontsize=40)
+ax1.set_title(r"\textbf{FASER}$\nu$ $\bar{\nu}_\mu$ \textbf{flux}", fontsize=40)
 
-fig1.savefig("../plots/FASERnu_component_fluxes.png", dpi=400)
-
-ax2.set_xlabel(r'\textbf{Neutrino Energy} (GeV)')
-ax2.set_ylabel(r'\textbf{Neutrinos} (1 / bin)')
-ax2.set_xscale('log')
-ax2.set_yscale('log')
-#ax2.set_ylim(1e6,1e12)
-#ax2.set_xlim(1e1,5e4)
-ax2.legend(loc='upper right')
-ax2.set_title(r"\textbf{FASER}$\nu$ $\nu_\mu$ \textbf{flux - Toni Makela}", fontsize=40)
-
-fig2.savefig("../plots/FASERnu_Toni.png", dpi=400)
+fig1.savefig("../plots/FASERnu_vmubar_component_fluxes.png", dpi=400)
 
 ### Export histogram ###
 middle_energies = []
@@ -203,9 +138,8 @@ for i in range(len(bins_pion)-1):
 
 print(len(bins_pion), len(middle_energies), len(hist_total))
 print("Total entries: ", total_entries)
-print("Total entries - Toni: ", total_entries_toni)
 
-with open('FASERvmu.csv','w',newline='') as csvfile:
+with open('FASERvmubar.csv','w',newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for energy, flux in zip(middle_energies, hist_total):
         writer.writerow([energy,flux])
--- analysis/plot_form_factors.py
+++ analysis/plot_form_factors.py
@@ -51,6 +51,13 @@ FF_n_e = []
 FF_n_m = []
 FF_n_calc = []
 
+Q2s_Fe = []
+Us_Fe = []
+Q_Fe_CXX = []
+FF_Fe_CXX = []
+Q_Fe = []
+FF_Fe = []
+
 ### Read CXX file ###
 #with open('../csv/form_factors/argon.csv','r') as csvfile:
 #    data = csv.reader(csvfile, delimiter = ',')
@@ -76,10 +83,20 @@ FF_n_calc = []
 #        Q2s_n.append(q2)
 #        Us_n.append(u)
 #
+
+with open('../csv/form_factors/iron.csv','r') as csvfile:
+    data = csv.reader(csvfile, delimiter = ',')
+    for row in data:
+        q2 = float(row[0])
+        u = float(row[1])
+        Q2s_Fe.append(q2)
+        Us_Fe.append(u)
+
 #### Calculate form factor from data and store ###
 #FF2_array_Ar = np.gradient(Us_Ar, Q2s_Ar)
 #FF2_array_p = np.gradient(Us_p, Q2s_p)
 #FF2_array_n = np.gradient(Us_n, Q2s_n)
+FF2_array_Fe = np.gradient(Us_Fe, Q2s_Fe)
 #
 #for i in range(len(Q2s_Ar)):
 #    q = np.sqrt(Q2s_Ar[i])
@@ -98,6 +115,11 @@ FF_n_calc = []
 #    ff = np.sqrt(-FF2_array_n[i])
 #    Q_n.append(q)
 #    FF_n.append(ff)
+for i in range(len(Q2s_Fe)):
+    q = np.sqrt(Q2s_Fe[i])
+    ff = np.sqrt(-FF2_array_Fe[i])
+    Q_Fe_CXX.append(q)
+    FF_Fe_CXX.append(ff)
 
 ### Read digitized data and store ###
 #with open('../csv/form_factors/FF_Ar_FourierBessel_Alt.csv') as csvfile:
@@ -146,67 +168,151 @@ with open('../csv/form_factors/FF_n_magnetic.csv') as csvfile:
         Q_n_m.append(float(row[0]))
         FF_n_m.append(float(row[1]))
 
+with open('../csv/form_factors/FF_Fe_3Gp-orange_Alt.csv') as csvfile:
+    data = csv.reader(csvfile, delimiter = ',')
+    for row in data:
+        Q_Fe.append(float(row[0]))
+        FF_Fe.append(float(row[1]))
+
 #for i, Q in enumerate(Q_p_e):
 #    FF_p_calc.append(np.sqrt(FF_p_e[i]**2 + Q**2/(4*Mproton**2)*FF_p_n[i]**2))
 #
 #for i, Q in enumerate(Q_p_e):
 #    FF_p_calc.append(np.sqrt(FF_p_e[i]**2 + Q**2/(4*Mproton**2)*FF_p_n[i]**2))
 
+###################################
+##### Woods-Saxon Form Factor #####
+###################################
+
+### Form factor function from Woods-Saxon distribution ###
+fm_to_invGeV = 5.068 # 1 fm = 5.068 GeV^-1
+a = 0.523 * fm_to_invGeV # fm -> GeV^-1
+
+def WS_form_factor(A, Q):
+    if Q == 0:   # First value of Q2s is 0.0 which will lead to division by zero when integrating
+        Q = 0.00001
+    r0 = 1.126 * A**(1/3) * fm_to_invGeV   # Form factor should be dimensionless. Convert fm to GeV^-1.
+    Q2 = Q*Q
+
+    # WS form factor can be written analytically; see (A.3) in Ballett et al.
+    WS = (3 * np.pi * a) / (r0**2 + np.pi**2 * a**2)
+
+    WS *= np.pi * a * np.tanh(np.pi * Q * a)**(-1) * np.sin(Q * r0) - r0 * np.cos(Q * r0)
+
+    WS /= Q * r0 * np.sinh(np.pi * Q * a)
+
+    return WS
+
+### Calculate form factors for tungsten ###
+A_W = 184
+
+# Normalize to F(0) = 1; use 0.001 instead.
+W_norm  = WS_form_factor(A_W, 0.001)
+
+# Q is in GeV
+FF_W = [np.abs( WS_form_factor(A_W, Q) / W_norm)**2 for Q in Q_Ar]
+
+
 ##################
 #### Plotting ####
 ##################
 
-fig1, ax1 = plt.subplots(1, 1, tight_layout=True)
-fig2, ax2 = plt.subplots(1, 2, figsize=(26,13), tight_layout=True, sharex=True, sharey=True)
+fig1, ax1 = plt.subplots(1, 1, tight_layout=True) # Argon form factor
+fig2, ax2 = plt.subplots(1, 2, figsize=(26,13), tight_layout=True, sharex=True, sharey=True) # Proton and neutron form factors
+#fig2, ax2 = plt.subplots(1, 2, figsize=(26,13), tight_layout=True, sharex=True, sharey=True, gridspec_kw={'wspace':0})
+fig3, ax3 = plt.subplots(1, 1, tight_layout=True) # Tungsten form factor
+fig4, ax4 = plt.subplots(1, 1, tight_layout=True) # Iron form factor
 
 ### Plot 3-Fermi parameter form factor from CXX code
 ax1.plot(Q_Ar, FF_Ar, '-', color = 'royalblue', label='_hidden', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
 
 ### Plot proton and neutron magnetic form factors
-ax2[0].plot(Q_p_e, FF_p_e, '-', color = 'green', label='Electric', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
+ax2[0].plot(Q_p_e, FF_p_e, '-', color = '#A71342', label='Electric', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
 ax2[0].plot(Q_p_m, FF_p_m, '-', color = 'orange', label='Magnetic', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
 #ax2[0].plot(Q_p, FF_p, '--', color = 'royalblue', label='CXX', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
 
-ax2[1].plot(Q_n_e, FF_n_e, '-', color = 'green', label='Electric', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
+ax2[1].plot(Q_n_e, FF_n_e, '-', color = '#A71342', label='Electric', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
 ax2[1].plot(Q_n_m, FF_n_m, '-', color = 'orange', label='Magnetic', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
 #ax2[1].plot(Q_n, FF_n, '--', color = 'royalblue', label='CXX', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
+
+### Plot Woods-Saxon tungsten form factor 
+ax3.plot(Q_Ar, FF_W, '-', color = 'royalblue', label='_hidden', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
+
+### Plot CXX iron form factor 
+ax4.plot(Q_Fe, FF_Fe, '-', color = 'royalblue', label='_hidden', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
+#ax4.plot(Q_Fe_CXX, FF_Fe_CXX, '-', color = 'orange', label='_hidden', path_effects=[pe.Stroke(linewidth=6, foreground='k'), pe.Normal()])
+#ax4.text(0.69,5e-1,r'\textbf{3-parameter Gaussian charge distribution}', ha='right',fontsize=22)
 
 ### Styling and save ###
 # Scales #
 ax1.set_yscale('log')
 ax2[0].set_yscale('log')
 ax2[1].set_yscale('log')
+ax3.set_yscale('log')
+ax4.set_yscale('log')
 
 #Fixing ticks
-locmaj = matplotlib.ticker.LogLocator(base=10.0, subs=(1.0, ), numticks=1000)
-locmin = matplotlib.ticker.LogLocator(base=10.0, subs=np.arange(2, 10)*.1,numticks=2000)
-ax1.yaxis.set_major_locator(locmaj)
-ax1.yaxis.set_minor_locator(locmin)
-ax2[0].yaxis.set_major_locator(locmaj)
-ax2[0].yaxis.set_minor_locator(locmin)
+locmaj1 = matplotlib.ticker.LogLocator(base=10.0, subs=(1.0, ), numticks=500)
+locmin1 = matplotlib.ticker.LogLocator(base=10.0, subs=np.arange(2, 10)*.1,numticks=500)
+locmaj2 = matplotlib.ticker.LogLocator(base=10.0, subs=(1.0, ), numticks=500)
+locmin2 = matplotlib.ticker.LogLocator(base=10.0, subs=np.arange(2, 10)*.1,numticks=500)
+locmaj3 = matplotlib.ticker.LogLocator(base=10.0, subs=(1.0, ), numticks=500)
+locmin3 = matplotlib.ticker.LogLocator(base=10.0, subs=np.arange(2, 10)*.1,numticks=500)
+locmaj4 = matplotlib.ticker.LogLocator(base=10.0, subs=(1.0, ), numticks=500)
+locmin4 = matplotlib.ticker.LogLocator(base=10.0, subs=np.arange(2, 10)*.1,numticks=500)
+
+ax1.yaxis.set_major_locator(locmaj1)
+ax1.yaxis.set_minor_locator(locmin1)
+ax2[0].yaxis.set_major_locator(locmaj2)
+ax2[0].yaxis.set_minor_locator(locmin2)
+ax3.yaxis.set_major_locator(locmaj3)
+ax3.yaxis.set_minor_locator(locmin3)
+ax4.yaxis.set_major_locator(locmaj4)
+ax4.yaxis.set_minor_locator(locmin4)
 
 ax1.set_ylim(1e-4,1)
 ax1.set_xlim(0,0.7)
 
+ax2[0].set_xlim(0,3)
+ax2[1].set_xlim(0,3)
+
+ax3.set_xlim(0,0.7)
+ax3.set_ylim(1e-8,1)
+
+ax4.set_xlim(0,0.7)
+ax4.set_ylim(1e-4,1)
+
 # Axis labels #
 ax1.set_xlabel(r'\textbf{Momentum Transfer} $Q$ (GeV)')
-ax1.set_ylabel(r'$|F(Q^2)|$')
+ax1.set_ylabel(r'\textbf{Form Factor} $|F(Q^2)|$')
 ax1.set_title(r'\textbf{Argon Nuclear Form Factor}')
 
 ax2[0].set_xlabel(r'\textbf{Momentum Transfer} $Q$ (GeV)')
 ax2[1].set_xlabel(r'\textbf{Momentum Transfer} $Q$ (GeV)')
-ax2[0].set_ylabel(r'$|F(Q^2)|$')
+ax2[0].set_ylabel(r'\textbf{Form Factor} $|F(Q^2)|$')
 ax2[0].set_title(r'\textbf{Proton Form Factors}')
 ax2[1].set_title(r'\textbf{Neutron Form Factors}')
 
 ax2[0].legend(loc='upper right')
 ax2[1].legend(loc='upper right')
 
+ax3.set_xlabel(r'\textbf{Momentum Transfer} $Q$ (GeV)')
+ax3.set_ylabel(r'\textbf{Form Factor} $|F(Q^2)|$')
+ax3.set_title(r'\textbf{Tungsten Nuclear Form Factor}')
+
+ax4.set_xlabel(r'\textbf{Momentum Transfer} $Q$ (GeV)')
+ax4.set_ylabel(r'\textbf{Form Factor} $|F(Q^2)|$')
+ax4.set_title(r'\textbf{Iron Nuclear Form Factor}')
+
 # Grids #
 ax1.grid(which = 'major', axis = 'both')
 ax2[0].grid(which= 'major', axis = 'both')
 ax2[1].grid(which= 'major', axis = 'both')
+ax3.grid(which = 'major', axis = 'both')
+ax4.grid(which = 'major', axis = 'both')
 
 # Save #
-fig1.savefig("../plots/Ar_form_factor.png", dpi=400, bbox_inches='tight')
-fig2.savefig("../plots/nucleon_form_factors.png", dpi=400, bbox_inches='tight')
+fig1.savefig("../plots/form_factor_argon.png", dpi=100, bbox_inches='tight')
+fig2.savefig("../plots/form_factor_nucleons.png", dpi=100, bbox_inches='tight')
+fig3.savefig("../plots/form_factor_tungsten.png", dpi=100, bbox_inches='tight')
+fig4.savefig("../plots/form_factor_iron.png", dpi=100, bbox_inches='tight')
